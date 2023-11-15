85c1c8a353d0e72806cef76f826774fc527940b3
Praveen S R <praveentiger1997@gmail.com>
2023-11-15 23:41:03 +0530

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+let myDouble = 3.
+let myArray = [|1.0;2;1|]
+let myList = [1.0;2;1]
+printfn "%A" myList
+
+let newList = myList @ [1..5]
+printfn "%A" newList
+
+let Square x = x*x
+printfn "%i" (Square 2)
+let append (x, y) : string = x+y
+printfn "%s" (append("yes ","sir"))
+
+let add x y  = x + y
+let res1 = add "a" "b"
